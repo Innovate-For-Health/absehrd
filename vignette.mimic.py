ef37bbf5caa9464e533d04da0ddc62bf7ee17bef
hhunterzinck <haleyhunterzinck@gmail.com>
2020-09-24 17:29:47 -0700

    save corgan model to file

--- vignette.mimic.py
+++ vignette.mimic.py
@@ -4,6 +4,16 @@ from preprocessor import preprocessor
 from corgan import corgan
 from report import report
 from privacy import privacy
+import pickle
+from os.path import isfile
+
+def save_obj(obj, file_name):
+    with open(file_name, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(file_name):
+    with open(file_name, 'rb') as f:
+        return pickle.load(f)
 
 def main():
     
@@ -11,10 +21,15 @@ def main():
     outcome = 'died_90d'
     file_label = 'mimic'
     file_csv_real = '../data/'+file_label+'.raw.csv'
-    file_pdf = '../plots/vignette.'+file_label+'.description_report.pdf'
+    file_desc_pdf = '../plots/vignette.'+file_label+'.description_report.pdf'
+    file_pred_pdf = '../plots/vignette.'+file_label+'.prediction_report.pdf'
     file_csv_corgan = '../output/vignette.'+file_label+'.corgan.csv'
+    file_model = '../output/vignette.'+file_label+'.corgan.pkl'
     missing_value = '-999999'
     delim = '__'
+    n_epoch = 100
+    n_epoch_pre = 100
+    use_saved_model = True
     
     pre = preprocessor(missing_value=missing_value)
     rep = report(missing_value=missing_value)
@@ -42,8 +57,15 @@ def main():
     a_trn = np.array(a_trn)
     a_tst = np.array(a_tst)
 
+    # train generator
+    if isfile(file_model) and use_saved_model:
+        model = load_obj(file_model)
+    else:
+        model = cor.train(x=r_trn, n_cpu=16, debug=True, n_epochs=n_epoch, 
+                          n_epochs_pretrain=n_epoch_pre)
+        save_obj(model, file_model)
+    
     # generate synthetic data
-    model = cor.train(x=r_trn, n_cpu=16, debug=True, n_epochs=100)
     s = cor.generate(model, n_gen=len(r_tst))
     n_subset_s = round(len(s)*0.75)
     s_trn, s_tst = random_split(s, [n_subset_s, len(s)-n_subset_s])
@@ -59,22 +81,29 @@ def main():
     # report
     idx_outcome = np.unique(np.append(np.where(d['header']==outcome), np.where(d['header']==outcome+delim+outcome)))
     outcome_label = d['header'][idx_outcome][0]
-    report_status = rep.description_report(r_trn=r_trn, r_tst=r_tst, s=s, col_names=d['header'], 
-                             outcome=outcome_label, file_pdf=file_pdf, n_epoch=100, 
+    report_status_desc = rep.description_report(r_trn=r_trn, r_tst=r_tst, s=s, col_names=d['header'], 
+                             outcome=outcome_label, file_pdf=file_desc_pdf, n_epoch=100, 
+                             model_type='lr')
+    report_status_pred = rep.prediction_report(r_trn=r_trn, r_tst=r_tst, s=s, col_names=d['header'], 
+                             outcome=outcome_label, file_pdf=file_pred_pdf, n_epoch=100, 
                              model_type='lr')
     
-    mem_inf = pri.membership_inference(r_trn=r_trn, r_tst=r_tst, s_trn=s_trn, s_tst=s_tst,
-                             a_trn=a_trn, a_tst=a_tst, model_type='svm')
+    #mem_inf = pri.membership_inference(r_trn=r_trn, r_tst=r_tst, s_trn=s_trn, s_tst=s_tst,
+    #                        a_trn=a_trn, a_tst=a_tst, model_type='svm')
     
     # summary
-    if report_status:
-        print('Report written to', file_pdf)
+    if report_status_desc:
+        print('Description report written to', file_desc_pdf)
     else:
-        print('Error: report generation failed')
+        print('Error: description report generation failed')
+    if report_status_pred:
+        print('Description report written to', file_pred_pdf)
+    else:
+        print('Error: prediction report generation failed')
     print('Real dataset written to', file_csv_real)
     print('Synthetic dataset written to', file_csv_corgan)
-    print('AUC for authentic-synthetic: ', mem_inf['auc_as'])
-    print('AUC for real train-test: ', mem_inf['auc_rr'])
+    #print('AUC for authentic-synthetic: ', mem_inf['auc_as'])
+    #print('AUC for real train-test: ', mem_inf['auc_rr'])
     
 if __name__ == "__main__":
     main()
