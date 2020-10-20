1590bb4094577700d19e7301518d490dcbe6035b
hhunterzinck <haleyhunterzinck@gmail.com>
2020-10-20 16:16:44 -0700

    added subparsers for tasks

--- sehrd.py
+++ sehrd.py
@@ -14,58 +14,106 @@ from report import report
 choices_train_type = ['corgan']
 choices_report_type = ['prediction','description']
 
-# command line interface setup
+# parsers
 parser = argparse.ArgumentParser()
 
-# file paths and prefixes
-parser.add_argument('-i', '--infile_prefix', help='prefix to input files', type=str)
-parser.add_argument('-o', '--outfile_prefix', help='prefix to output files', type=str)
+# global parameters
 
-# flags
-parser.add_argument('-t', '--train', help='train a generative model', action='store_true')
-parser.add_argument('-g', '--generate', help='generate synthetic samples', action='store_true')
-parser.add_argument('-r', '--report', help='generate a synthetic data report', action='store_true')
-parser.add_argument('-v', '--verbose', help='print verbose output', action='store_true')
 
-# scenario options
-parser.add_argument('--train_type', 
+# subparsers
+subparsers = parser.add_subparsers(help='task to perform', 
+                                   dest='task',
+                                   required=True)
+parser_t = subparsers.add_parser('train', 
+                    help='train a synthetic data generator')
+parser_g = subparsers.add_parser('generate', 
+                    help='generate synthetic data samples')
+parser_r = subparsers.add_parser('report', 
+                    help='report on synthetic data sample')
+
+# subparser: train
+parser_t.add_argument('--file_data', 
+                    help='path to file containing real data', 
+                    type=str,
+                    required=True)
+parser_t.add_argument('--outprefix_model', 
+                    help='file prefix to model pkl dump', 
+                    type=str, 
+                    required=True)
+parser_t.add_argument('--train_type', 
                     help='type of generative model to train', 
                     choices=choices_train_type,
-                    default=choices_train_type[0])
-parser.add_argument('--generate_size', 
-                    help='number of synthetic samples to generate (>0)', 
-                    type=int,
-                    default=1000)
-parser.add_argument('--report_type', 
-                    help='type of synthetic data report to generate', 
-                    choices=choices_report_type,
-                    default=choices_report_type[0])
-
-# other options
-parser.add_argument('--n_cpu', 
-                    help='number of CPUs to use (>0)', 
-                    type=int,
-                    default=1)
-parser.add_argument('--n_epoch_pre', 
+                    default=choices_train_type[0],
+                    required=False)
+parser_t.add_argument('--n_epoch_pre', 
                     help='number of pre-training epochs (>0)', 
                     type=int,
-                    default=100)
-parser.add_argument('--n_epoch', 
+                    default=100,
+                    required=False)
+parser_t.add_argument('--n_epoch', 
                     help='number of training epochs (>0)', 
                     type=int,
-                    default=100)
-parser.add_argument('--missing_value',
+                    default=100,
+                    required=False)
+parser_t.add_argument('--missing_value',
                     help='representation of missing value',
                     type=str,
-                    default='-999999')
-parser.add_argument('--outcome',
-                    help='column name of feature to be used as prediction target',
-                    type=str,
-                    default='outcome')
-parser.add_argument('--frac_train', 
+                    default='-999999',
+                    required=False)
+parser_t.add_argument('--frac_train', 
                     help='fraction of real data to train generative model', 
                     type=float,
-                    default=0.75)
+                    default=0.75,
+                    required=False)
+parser_t.add_argument('-v', '--verbose', 
+                    help='print verbose output', 
+                    action='store_true', 
+                    required=False)
+parser_t.add_argument('--n_cpu_train', 
+                    help='number of CPUs to use (>0)', 
+                    type=int, 
+                    default=1,
+                    required=False)
+
+# subparser: generate
+parser_g.add_argument('--file_model', 
+                    help='path to file containing trained generator', 
+                    type=str,
+                    required=True)
+parser_g.add_argument('-o', '--outprefix_synth', 
+                    help='file prefix to synthetic data CSV', 
+                    type=str, 
+                    required=True)
+parser_g.add_argument('--generate_size', 
+                    help='number of synthetic samples to generate (>0)', 
+                    type=int,
+                    default=1000,
+                    required=False)
+parser_g.add_argument('-v', '--verbose', 
+                    help='print verbose output', 
+                    action='store_true', 
+                    required=False)
+parser_g.add_argument('--n_cpu_generate', 
+                    help='number of CPUs to use (>0)', 
+                    type=int, 
+                    default=1,
+                    required=False)
+
+# subparser: report
+parser_r.add_argument('--outprefix_report', 
+                    help='file prefix to report PDF', 
+                    type=str, 
+                    required=True)
+parser_r.add_argument('--report_type', 
+                    help='type of synthetic data report to generate', 
+                    choices=choices_report_type,
+                    default=choices_report_type[0],
+                    required=False)
+parser_r.add_argument('--outcome',
+                    help='column name of feature to be used as prediction target',
+                    type=str,
+                    default='outcome',
+                    required=False)
 
 # check user input -------------
 
@@ -74,57 +122,87 @@ if len(sys.argv)==1:
     parser.print_help()
     sys.exit(0)
     
-# get command lines
+# get command line arguments
 args = parser.parse_args()
+print(args)
 
-# check user input
-if args.generate_size <= 0:
-    parser.print_usage()
-    print('sehrd.py: error: argument --generate_size: invalid choice: \'' +
-          str(args.generate_size) + '\' (choose integer greater than 0)')
-    sys.exit(0)
-if args.n_epoch_pre <= 0:
-    parser.print_usage()
-    print('sehrd.py: error: argument --n_epoch_pre: invalid choice: \'' +
-          str(args.n_epoch_pre) + '\' (choose integer greater than 0)')
-    sys.exit(0)
-if args.n_epoch <= 0:
-    parser.print_usage()
-    print('sehrd.py: error: argument --n_epoch: invalid choice: \'' +
-          str(args.n_epoch) + '\' (choose integer greater than 0)')
-    sys.exit(0)
-max_cpu = multiprocessing.cpu_count()
-if args.n_cpu < 1 or args.n_cpu > max_cpu:
-    parser.print_usage()
-    print('sehrd.py: error: argument --n_cpu: invalid choice: \'' +
-          str(args.n_cpu) + '\' (choose integer in range [1,' + 
-          str(max_cpu) + '])')
-    sys.exit(0)
-if args.frac_train < 0 or args.frac_train > 1:
-    parser.print_usage()
-    print('sehrd.py: error: argument --frac_train: invalid choice: \'' +
-          str(args.n_cpu) + '\' (choose float in range [0,1])')
-    sys.exit(0)
+
+# check arguments for 'train' task        
+if args.task == 'train':
+    
+    if args.n_epoch_pre <= 0:
+        parser.print_usage()
+        print('sehrd.py: error: argument --n_epoch_pre: invalid choice: \'' +
+              str(args.n_epoch_pre) + '\' (choose integer greater than 0)')
+        sys.exit(0)
+    
+    if args.n_epoch <= 0:
+        parser.print_usage()
+        print('sehrd.py: error: argument --n_epoch: invalid choice: \'' +
+              str(args.n_epoch) + '\' (choose integer greater than 0)')
+        sys.exit(0)
+    
+    max_cpu = multiprocessing.cpu_count()
+    if args.n_cpu_train < 1 or args.n_cpu_train > max_cpu:
+        parser.print_usage()
+        print('sehrd.py: error: argument --n_cpu_train: invalid choice: \'' +
+              str(args.n_cpu_train) + '\' (choose integer in range [1,' + 
+              str(max_cpu) + '])')
+        sys.exit(0)
+    
+    if args.frac_train < 0 or args.frac_train > 1:
+        parser.print_usage()
+        print('sehrd.py: error: argument --frac_train: invalid choice: \'' +
+              str(args.n_cpu) + '\' (choose float in range [0,1])')
+        sys.exit(0)
+    
+    if not isfile(args.file_data):
+        parser.print_usage()
+        print('sehrd.py: error: argument --file_data: file does not exist: \'' +
+              str(args.file_data) + '\' (check path to file)')
+        sys.exit(0)
+        
+# check arguments for 'generate' task
+if args.task == 'generate':
+    
+    if args.generate_size <= 0:
+        parser.print_usage()
+        print('sehrd.py: error: argument --generate_size: invalid choice: \'' +
+              str(args.generate_size) + '\' (choose integer greater than 0)')
+        sys.exit(0)
+        
+    if not isfile(args.file_model):
+        parser.print_usage()
+        print('sehrd.py: error: argument --file_model: file does not exist: \'' +
+              str(args.file_model) + '\' (check path to file)')
+        sys.exit(0)
+        
+    max_cpu = multiprocessing.cpu_count()
+    if args.n_cpu_generate < 1 or args.n_cpu_generate > max_cpu:
+        parser.print_usage()
+        print('sehrd.py: error: argument --n_cpu_generate: invalid choice: \'' +
+              str(args.n_cpu_generate) + '\' (choose integer in range [1,' + 
+              str(max_cpu) + '])')
+        sys.exit(0)
+        
+
+    
+# check for required parameters for each scenario
+"""
     
 # scenarios --------------
 
 # initilize
-pre = preprocessor(missing_value=args.missing_value)
-rep = report(missing_value=args.missing_value)
+# TODO: need to move these initializations to appropriate blocks
+#       and deal with missing value and report...
+pre = preprocessor(missing_value=args_t.missing_value)
+rep = report(missing_value=args_t.missing_value)
 cor = corgan()
 
-# loading real data, if applicable
-if args.generate and not (args.train or args.report):
-    if(args.verbose):
-        print('Skipping real data loading')
-else:
-    
-    # check that args.file_real exists
-    if not isfile(args.file_real):
+if args_m.task == 'train':
         
-    
     # load real data
-    ftr = pre.read_file(args.file_real, has_header=True)
+    ftr = pre.read_file(args.file, has_header=True)
     header = ftr['header']
     x = ftr['x']
         
@@ -140,47 +218,9 @@ else:
     r_trn = r[idx_trn,:]
     r_tst = r[idx_tst,:]
 
-    
-if args.train and args.generate and args.report: 
-    
-    if args.verbose:
-        print('Scenario 1: train, generate, report')
-        
-    
-    
-    # train
-    
-    # generate
-    
-    # report
+# train
 
-elif args.train and args.generate:
-    
-    if args.verbose:
-        print('Scenario 2: train, generate')
-        
-elif args.generate and args.report: 
-    
-    if args.verbose:
-        print('Scenario 3: generate, report')
-        
-elif args.train and args.report: 
-    
-    if args.verbose:
-        print('Scenario 4: train, report')
-        
-elif args.train: 
-    
-    if args.verbose:
-        print('Scenario 5: train')
-        
-elif args.generate: 
-    
-    if args.verbose:
-        print('Scenario 6: generate')
+# generate
 
-elif args.report: 
-    
-    if args.verbose:
-        print('Scenario 7: report')
-        
+# report
+"""
